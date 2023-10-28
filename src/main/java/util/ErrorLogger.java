107b55f608933f30f9ede63da1b2fa127af43850
Chamod Perera <chamod914@gmail.com>
2023-10-28 22:45:48 +0530

    ErrorLogger

---
+++ src/main/java/util/ErrorLogger.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class ErrorLogger {
+	
+	public static final  Logger log = Logger.getLogger(ErrorLogger.class.getName());
+	
+	
+	public static void getLogger() {
+		FileHandler fh;
+		
+		try {
+			fh = new FileHandler("G:/Final/ErrorLogger/logfile.log");
+			
+			log.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+			
+			log.info("Logger Initialized");
+			
+			
+		} catch (SecurityException e) {
+			log.log(Level.SEVERE, "Security Exception :: " , e);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			log.log(Level.SEVERE, "IO Exception :: " , e);
+		}
+	}
+	
+	public static void main(String[] args) {
+	    getLogger(); // Initialize the logger
+	    
+	    try {
+	        // Attempt to open a non-existent file
+	        FileInputStream fileInputStream = new FileInputStream("non_existent_file.txt");
+	    } catch (IOException e) {
+	        log.log(Level.SEVERE, "IO Exception occurred", e);
+	    }
+	}
+
+	
+}
