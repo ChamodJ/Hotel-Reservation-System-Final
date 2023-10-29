4aff273153896c73ad36eae824ee41225e21dbd0
Chamod Perera <chamod914@gmail.com>
2023-10-29 21:34:10 +0530

    Update

--- src/main/java/servlet/UpdateReservationServlet.java
+++ src/main/java/servlet/UpdateReservationServlet.java
@@ -2,6 +2,10 @@ package servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -41,6 +45,8 @@ public class UpdateReservationServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		// Getting data from the form
+		SimpleDateFormat cDate = new SimpleDateFormat("yyyy-MM-dd");
+		
 		int Reservaton_id = Integer.parseInt(req.getParameter("Reservaton_id"));
 		int User_id = Integer.parseInt(req.getParameter("User_id"));
 		int Hotel_id = Integer.parseInt(req.getParameter("Hotel_id"));
@@ -49,21 +55,39 @@ public class UpdateReservationServlet extends HttpServlet {
 		String check_out_date = req.getParameter("check_out_date");
 		int No_of_Gusts = Integer.parseInt(req.getParameter("No_of_Gusts"));
 		float Amount = Float.parseFloat(req.getParameter("Amount"));
-
-		// Creating a Hotel object with the received data
-		Booking booking = new Booking(Reservaton_id, User_id, Hotel_id, room_id, check_in_date, check_out_date,
-				No_of_Gusts, Amount);
-
-		try {
+		
+		
+			java.util.Date checkInDate;
+			try {
+				checkInDate = cDate.parse(check_in_date);
+				java.util.Date checkOutDate = cDate.parse(check_out_date);
+				
+				Date checkIDate = new Date(checkInDate.getTime());
+				Date checkODate = new Date(checkOutDate.getTime());
+				
+				// Creating a Hotel object with the received data
+				Booking booking = new Booking();
+				
+				booking.setReservaton_id(Reservaton_id);
+				booking.setUser_id(User_id);
+				booking.setHotel_id(Hotel_id);
+				booking.setRoom_id(room_id);
+				booking.setCheck_in_date(checkIDate);
+				booking.setCheck_out_date(checkODate);
+				booking.setNo_of_Gusts(No_of_Gusts);
+				booking.setAmount(Amount); 
+		
 			// Creating a HotelDAO object with database connection
 			ReservationServices rs = new ReservationServices(DBConnectionUtil.getDBConnection());
-System.out.println(booking);
 			// Updating the hotel information in the database
 			rs.updateReservation(booking);
 
 			// Redirecting to the Previous page after the update
 			resp.sendRedirect("AdminReservation.jsp");
 		} catch (ClassNotFoundException | SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
